fe1b1c93a75526c8faf1e9dcb4a1173ac2b0a969
vijju <vijju@vijjus-MacBook-Air.local>
2024-07-28 23:53:57 +0530

    update port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1314;
 
 app.listen(port, () => {
   console.log(`Tracking server running at http://localhost:${port}`);
