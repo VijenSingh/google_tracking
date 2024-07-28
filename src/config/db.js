7a99c3eb2ad7a87bfda9d6af63aef35ed1dc5219
vijju <vijju@vijjus-MacBook-Air.local>
2024-07-29 00:19:54 +0530

    update db

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 const mongoURI = process.env.MONGODB_URI;
 
+console.log('Environment Variables:');
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+console.log('ALLOWED_ORIGINS:', process.env.ALLOWED_ORIGINS); 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true,serverSelectionTimeoutMS: 30000,socketTimeoutMS: 45000 })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
