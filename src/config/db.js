aab80746291b05f0d5d1c4c911bed3160590d508
vijju <vijju@vijjus-MacBook-Air.local>
2024-07-28 21:51:31 +0530

    undo trackController

--- src/config/db.js
+++ src/config/db.js
@@ -4,11 +4,6 @@ dotenv.config();
 
 const mongoURI = process.env.MONGODB_URI;
 
-if (!mongoURI) {
-  console.error('MongoDB connection error: MONGODB_URI environment variable is not set');
-  process.exit(1);
-}
-
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
